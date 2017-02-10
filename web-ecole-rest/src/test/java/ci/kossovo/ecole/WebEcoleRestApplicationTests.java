c168e246b6d0c054b76038c246330f17caec274e
Diarrassouba Drissa <ddrissaci@gmail.com>
2017-02-10 15:59:14 +0000

    Creation test mvc get(/personnes)

--- web-ecole-rest/src/test/java/ci/kossovo/ecole/WebEcoleRestApplicationTests.java
+++ web-ecole-rest/src/test/java/ci/kossovo/ecole/WebEcoleRestApplicationTests.java
@@ -1,17 +1,59 @@
-/*package ci.kossovo.ecole;
+package ci.kossovo.ecole;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ci.kossovo.ecole.entity.Personne;
+import ci.kossovo.ecole.web.models.personne.ApplicationModelPersonne;
+import ci.kossovo.ecole.web.services.PersonneRestService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(PersonneRestService.class)
 public class WebEcoleRestApplicationTests {
 
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private ApplicationModelPersonne personneModel;
+	
+	private ObjectMapper mapper= new ObjectMapper();
+
 	@Test
-	public void contextLoads() {
+	public void trouverToutesPersonnes() throws Exception {
+		List<Personne> personnes= new ArrayList<>();
+		Personne p1= new Personne("Mr", "Diarra", "Drissa");
+		p1.setId(1L);
+		personnes.add(p1);
+		Personne p2= new Personne("Mr", "Traoré", "Abdoulaye");
+		p2.setId(2L);
+		personnes.add(p2);
+		
+		given(this.personneModel.findAll())
+		.willReturn(personnes);
+		
+		this.mvc.perform(get("/personnes"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.body.length()", is(2)))
+		.andExpect(jsonPath("$.body.[0].nom", is("Diarra")));
+		
 	}
 
 }
-*/
